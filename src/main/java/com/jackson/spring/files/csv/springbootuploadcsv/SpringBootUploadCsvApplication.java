22f4702240dfc0e9696a02288b5b2148e945cae8
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-04 11:26:30 -0300

    Leitura CSV

---
+++ src/main/java/com/jackson/spring/files/csv/springbootuploadcsv/SpringBootUploadCsvApplication.java
@@ -0,0 +1,13 @@
+package com.jackson.spring.files.csv.springbootuploadcsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackson/spring/files/csv/springbootuploadcsv/SpringBootUploadCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackson.spring.files.csv.springbootuploadcsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUploadCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
